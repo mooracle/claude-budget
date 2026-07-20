175b69b5ab123d5859d53b07ef08e257adf651cd
Alex Sergeyev <alex@mooracle.io>
2026-07-20 17:16:46 +0200

    release: v0.2.0
    Claude-Cost: 8.76

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ var pricingData []byte
 //go:embed hooks/prepare-commit-msg hooks/post-commit
 var hookFS embed.FS
 
-const version = "0.1.1"
+const version = "0.2.0"
 
 func main() {
 	if len(os.Args) < 2 {
